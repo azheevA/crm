f34685b4648390f89d64726cbae9fed1fe2d9c33
azheevA <sazheev@gmail.com>
2026-03-05 00:46:15 +0800

    fix

--- backend/src/tasks/tasks.module.ts
+++ backend/src/tasks/tasks.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ActivityModule } from 'src/activity/activity.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ActivityModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
